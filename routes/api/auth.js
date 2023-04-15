95f5917a1eb90f39635fbe7fab7c1c61a9e7f25e
Aleks <777777ot@gmail.com>
2023-04-16 00:28:24 +0200

    444

---
+++ routes/api/auth.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const { validateBody } = require("../../utils");
+
+const { schemas } = require("../../models/user");
+
+const router = express.Router();
+
+const ctrl = require("../../controllers/auth")
+
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login)
+
+module.exports = router;
